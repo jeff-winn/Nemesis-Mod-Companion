e9d0f2d7c327ca1e4b10ba9081cf6157f42d0933
Jeff Winn <6961614+jeff-winn@users.noreply.github.com>
2019-10-26 23:24:44 -0400

    Initial check-in.

---
+++ src/Nemesis-Mod-Companion/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Nemesis_Mod_Companion.Models;
+
+namespace Nemesis_Mod_Companion.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
